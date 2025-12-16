7fd58bf83cabb17e30a58394f14101f8eab08dfb
skilhouse <skilhouse@gmail.com>
2025-12-17 00:10:28 +0300

    Add properties and change WebDriverFactory

--- src/test/java/ru/yandex/praktikum/utils/WebDriverFactory.java
+++ src/test/java/ru/yandex/praktikum/utils/WebDriverFactory.java
@@ -5,17 +5,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class WebDriverFactory {
 
+    private static final Properties config = loadConfig();
+
+    private static Properties loadConfig() {
+        Properties props = new Properties();
+        try (InputStream input = WebDriverFactory.class
+                .getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            if (input != null) {
+                props.load(input);
+            }
+        } catch (Exception e) {
+            System.out.println("Конфиг не загружен, используем значения по умолчанию");
+        }
+        return props;
+    }
+
     public static WebDriver getDriver() {
-        String browser = System.getProperty("browser", "chrome").toLowerCase();
+        String browser = System.getProperty("browser",
+                config.getProperty("browser.default", "chrome"));
+
         System.out.println("Браузер: " + browser);
 
-        if ("yandex".equals(browser)) {
-            return getYandexDriver();
-        } else {
-            return getChromeDriver();
-        }
+        return "yandex".equalsIgnoreCase(browser) ? getYandexDriver() : getChromeDriver();
     }
 
     private static WebDriver getChromeDriver() {
@@ -26,15 +43,22 @@ public class WebDriverFactory {
     }
 
     private static WebDriver getYandexDriver() {
-        String yandexDriverPath = "C:/Users/user/yandexdriver.exe";
-        System.setProperty("webdriver.chrome.driver", yandexDriverPath);
+        String driverPath = config.getProperty("yandex.driver.path", "");
+        if (!driverPath.isEmpty()) {
+            System.setProperty("webdriver.chrome.driver", driverPath);
+        } else {
+            WebDriverManager.chromedriver().setup();
+        }
 
         ChromeOptions options = new ChromeOptions();
-        String yandexBrowserPath = "C:/Users/user/AppData/Local/Yandex/YandexBrowser/Application/browser.exe";
-        options.setBinary(yandexBrowserPath);
+        String browserPath = config.getProperty("yandex.browser.path", "");
+
+        if (!browserPath.isEmpty()) {
+            options.setBinary(browserPath);
+            System.out.println("Яндекс.Браузер: " + browserPath);
+        }
 
         options.addArguments("--no-sandbox", "--disable-dev-shm-usage", "--remote-allow-origins=*");
-
         return new ChromeDriver(options);
     }
 }
